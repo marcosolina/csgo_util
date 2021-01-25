1cfc71ced87f4ca1104772391ce73b13985c14c8
Marco Solina <Marco.Solina@ixico.com>
2021-01-25 23:49:35 +0100

    first commit

---
+++ CsgoRestApi/src/test/java/com/marco/csgorestapi/CsgoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.csgorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

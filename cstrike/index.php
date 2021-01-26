abeba54cc4bc626fa7c8972f30ec418fd19c62fe
Marco Solina <Marco.Solina@ixico.com>
2021-01-26 23:03:26 +0100

    refactoring

--- cstrike/index.php
+++ cstrike/index.php
@@ -42,15 +42,26 @@ function formatSizeUnits($bytes)
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="http://marco.selfip.net/cstrike" />
 	<meta property="og:image" content="csgo.jpg" />
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="./style.css">
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/animate.css">
+	<link rel="stylesheet" href="./css/pnotify.custom.min.css">
+	
 
 	<!-- Javascript -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+	<script src="./js/pnotify.custom.min.js"></script>
+	<script src="./js/MarcoUtils.js"></script>
+	<script src="./js/Csgo.js"></script>
+
+	<script>
+		$(document).ready(Csgo.init);
+	</script>
 
 </head>
 
@@ -75,6 +86,9 @@ function formatSizeUnits($bytes)
 				</li>
 				<li class="nav-item" role="presentation">
 					<a class="nav-link" id="todo-tab" data-toggle="tab" href="#todoDiv" role="tab" aria-controls="todo" aria-selected="false">Rounds</a>
+				</li>
+				<li class="nav-item" role="presentation">
+					<a class="nav-link" id="rcon-tab" data-toggle="tab" href="#rconDiv" role="tab" aria-controls="todo" aria-selected="false">Rcon</a>
 				</li>
 			</ul>
 			<div class="tab-content" id="myTabContent">
@@ -249,6 +263,58 @@ function formatSizeUnits($bytes)
 						?>
 					</div>
 					<!-- END round files Accordion -->
+				</div>
+				<div class="tab-pane fade" id="rconDiv" role="tabpanel" aria-labelledby="dem-tab">
+					<!-- START RCON -->
+					<br>
+					<div class="row">
+						<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="rconHostText">Rcon Host</span>
+								</div>
+								<input type="text" class="form-control" id="rconHost">
+							</div>
+						</div>
+						<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="rconPortText">Rcon Port</span>
+								</div>
+								<input type="text" class="form-control" id="rconPort">
+							</div>
+						</div>
+						<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="rconPasswText">Rcon Passw</span>
+								</div>
+								<input type="password" class="form-control" id="rconPassw">
+							</div>
+						</div>
+					</div>
+					<div class="rcon-container">
+						<?php
+							$filesRootFolder = "./rcon/maps";
+							$files = scandir($filesRootFolder, 1);
+							foreach ($files as $file) {
+								if ($file == "." || $file == "..") {
+									continue;
+								}
+								if (!is_dir($filesRootFolder . "/" . $file)) {
+						?>
+						<div class="card rcon-map" data-rcon-cmd="<?php echo "map ".str_replace(".jpg", "", $file);?>">
+							<img class="card-img-top" src="<?php echo $filesRootFolder . "/" . $file;?>" alt="Card image cap">
+							<div class="card-body">
+								<p class="card-text"><?php echo str_replace(".jpg", "", $file);?></p>
+							</div>
+						</div>
+						<?php 
+								}
+							}
+						?>
+					</div>
+					<!-- END RCON -->
 				</div>
 			</div>
 		</div>

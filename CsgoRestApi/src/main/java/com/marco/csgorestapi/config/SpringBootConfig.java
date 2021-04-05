92a18244ae980b70541a15cd418d78dd46ac988c
Marco Solina <Marco.Solina@ixico.com>
2021-04-05 12:04:54 +0200

    Adding some error messages

--- CsgoRestApi/src/main/java/com/marco/csgorestapi/config/SpringBootConfig.java
+++ CsgoRestApi/src/main/java/com/marco/csgorestapi/config/SpringBootConfig.java
@@ -1,7 +1,9 @@
 package com.marco.csgorestapi.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import com.marco.csgorestapi.services.implementations.RconServiceSteamCondenser;
 import com.marco.csgorestapi.services.interfaces.RconService;
@@ -18,5 +20,13 @@ public class SpringBootConfig {
 	@Bean
 	public RconService getRconService() {
 		return new RconServiceSteamCondenser();
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:/messages/errorCodes");
+
+		return messageSource;
 	}
 }
--- CsgoRestApi/src/main/java/com/marco/csgorestapi/services/implementations/RconServiceSteamCondenser.java
+++ CsgoRestApi/src/main/java/com/marco/csgorestapi/services/implementations/RconServiceSteamCondenser.java
@@ -4,6 +4,9 @@ import java.util.concurrent.TimeoutException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
 import com.github.koraktor.steamcondenser.steam.servers.GameServer;
@@ -13,9 +16,25 @@ import com.marco.utils.MarcoException;
 
 public class RconServiceSteamCondenser implements RconService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RconServiceSteamCondenser.class);
+	@Autowired
+	private MessageSource msgSource;
 
 	@Override
 	public String sendRconCommand(String host, int port, String rconPassw, String rconCmd) throws MarcoException {
+
+		if (host == null || host.isEmpty()) {
+			throw new MarcoException(msgSource.getMessage("CSGOAPI00001", null, LocaleContextHolder.getLocale()));
+		}
+		if (port == 0) {
+			throw new MarcoException(msgSource.getMessage("CSGOAPI00002", null, LocaleContextHolder.getLocale()));
+		}
+		if (rconPassw == null || rconPassw.isEmpty()) {
+			throw new MarcoException(msgSource.getMessage("CSGOAPI00003", null, LocaleContextHolder.getLocale()));
+		}
+		if (rconCmd == null || rconCmd.isEmpty()) {
+			throw new MarcoException(msgSource.getMessage("CSGOAPI00004", null, LocaleContextHolder.getLocale()));
+		}
+
 		try {
 			GameServer server = new SourceServer(host, port);
 			LOGGER.debug("Authenticating on the server");
@@ -25,7 +44,10 @@ public class RconServiceSteamCondenser implements RconService {
 			LOGGER.debug(String.format("RCON response: %s", response));
 			return response;
 		} catch (SteamCondenserException | TimeoutException e) {
-			throw new MarcoException(e);
+			if (LOGGER.isTraceEnabled()) {
+				e.printStackTrace();
+			}
+			throw new MarcoException(msgSource.getMessage("CSGOAPI00005", null, LocaleContextHolder.getLocale()));
 		}
 	}
 }

31ad75c400c286d2a02fd0670d66d878585dfab5
Mark Austin <mark.r.austin@gmail.com>
2021-04-10 15:59:25 +0100

    removed comments

--- DemParser/Program.cs
+++ DemParser/Program.cs
@@ -16,8 +16,8 @@ namespace DemParser
 		{
 			string demFile = ""; 
 			demFile = "C:\\demos\\auto0-20210405-190514-472545077-de_dust2-IXI-GO__Monday_Nights__Marco_.dem";
-			//demFile = "C:\\tmp\\demfiles\\2021-03-29\\auto0-20210329-193658-1526021228-cs_assault-IXI-GO__Monday_Nights__Marco_.dem";
-			//demFile = args[0];
+			demFile = "C:\\tmp\\demfiles\\2021-03-29\\auto0-20210329-193658-1526021228-cs_assault-IXI-GO__Monday_Nights__Marco_.dem";
+			demFile = args[0];
 
 			Demo demo = new Demo();
 			demo.Path = demFile;

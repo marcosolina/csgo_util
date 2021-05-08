1eff672813466812e9e798d22b4a0b70d501d84b
Marco Solina <Marco.Solina@ixico.com>
2021-05-08 23:08:42 +0200

    continuing the implementation

---
+++ DiscordBot/src/main/java/com/marco/discordbot/enums/EventType.java
@@ -0,0 +1,19 @@
+package com.marco.discordbot.enums;
+
+/**
+ * Types of events that the CSGO server can dispatch
+ * 
+ * @author Marco
+ *
+ */
+public enum EventType {
+    // @formatter:off
+    ROUND_END,
+    ROUND_START,
+    CS_WIN_PANEL_MATCH,
+    WARMUP_START,
+    WARMUP_END,
+    // @formatter:on
+    ;
+
+}
---
+++ DiscordBot/src/main/java/com/marco/discordbot/model/rest/roundparser/Teams.java
@@ -0,0 +1,15 @@
+package com.marco.discordbot.model.rest.roundparser;
+
+import java.util.List;
+
+public class Teams {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+}

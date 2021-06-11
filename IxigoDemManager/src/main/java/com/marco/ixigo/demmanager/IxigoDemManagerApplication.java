e8ed9a6b687f2123e25c0ea281fbff7c19b2dd08
Marco Solina <Marco.Solina@ixico.com>
2021-06-11 22:05:56 +0200

    refactoring

--- IxigoDemManager/src/main/java/com/marco/ixigo/demmanager/IxigoDemManagerApplication.java
+++ IxigoDemManager/src/main/java/com/marco/ixigo/demmanager/IxigoDemManagerApplication.java
@@ -2,8 +2,10 @@ package com.marco.ixigo.demmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IxigoDemManagerApplication {
 
 	public static void main(String[] args) {

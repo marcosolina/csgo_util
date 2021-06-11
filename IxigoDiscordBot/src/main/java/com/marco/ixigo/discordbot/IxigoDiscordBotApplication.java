4975a67ff12e8539cbd35876621e7990d8402efe
Marco Solina <Marco.Solina@ixico.com>
2021-06-11 21:50:25 +0200

    refactoring

---
+++ IxigoDiscordBot/src/main/java/com/marco/ixigo/discordbot/IxigoDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.marco.ixigo.discordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxigoDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxigoDiscordBotApplication.class, args);
+	}
+
+}
---
+++ IxigoDiscordBot/src/test/java/com/marco/ixigo/discordbot/IxigoDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.ixigo.discordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IxigoDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

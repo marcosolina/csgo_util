25dae94887b9e564d0d210291a437292bf967908
Marco Solina <Marco.Solina@ixico.com>
2021-06-12 09:12:52 +0200

    refactoring

---
+++ IxigoRconApiService/src/main/java/com/marco/ixigo/rconapiservice/model/rest/RconHttpResponse.java
@@ -0,0 +1,26 @@
+package com.marco.ixigo.rconapiservice.model.rest;
+
+import com.marco.utils.http.MarcoResponse;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * HTTP response model returned after forwarding the RCON command
+ * 
+ * @author Marco
+ *
+ */
+public class RconHttpResponse extends MarcoResponse {
+
+    @ApiModelProperty(notes = "The RCON response (if any)", required = false)
+    private String rconResponse;
+
+    public String getRconResponse() {
+        return rconResponse;
+    }
+
+    public void setRconResponse(String rconResponse) {
+        this.rconResponse = rconResponse;
+    }
+
+}

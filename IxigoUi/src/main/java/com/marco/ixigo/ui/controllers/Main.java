b867a3f59100cdda7c593e2e1f265f6be867cba7
Marco Solina <Marco.Solina@ixico.com>
2021-06-19 13:40:59 +0200

    refactoring

--- IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
+++ IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
@@ -1,5 +1,8 @@
 package com.marco.ixigo.ui.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +12,25 @@ import com.marco.ixigo.ui.config.Urls;
 
 @Controller
 public class Main {
-    
+
     @Autowired
     private Urls urls;
-    
+
     @RequestMapping
     public String landingPage(Model model) {
-        model.addAttribute("URLS", urls);
+        model.addAttribute("URLS", capitaliseAllKeys());
         return "index";
+    }
+
+    private Map<String, Map<String, String>> capitaliseAllKeys() {
+        Map<String, Map<String, String>> map = new HashMap<>();
+        urls.getUrls().forEach((k, v) -> {
+            Map<String, String> inner = new HashMap<>();
+
+            v.forEach((k2, v2) -> inner.put(k2.toUpperCase(), v2));
+            map.put(k.toUpperCase(), inner);
+        });
+
+        return map;
     }
 }

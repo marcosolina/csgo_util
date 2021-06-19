5776026a8c88fa00af1437174995d24f98f532a3
Marco Solina <Marco.Solina@ixico.com>
2021-06-19 10:43:41 +0200

    refactoring

---
+++ IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
@@ -0,0 +1,12 @@
+package com.marco.ixigo.ui.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Main {
+    @RequestMapping
+    public String landingPage() {
+        return "index";
+    }
+}

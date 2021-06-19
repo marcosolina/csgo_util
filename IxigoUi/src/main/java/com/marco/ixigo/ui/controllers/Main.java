8c67b316220de3392304585aeec3a532bc4f9a98
Marco Solina <Marco.Solina@ixico.com>
2021-06-19 14:45:01 +0200

    refactoring

--- IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
+++ IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.marco.ixigo.ui.config.Urls;

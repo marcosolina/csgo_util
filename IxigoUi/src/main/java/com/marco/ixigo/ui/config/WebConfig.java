ca36f7ad86af0dcde4c3679060620a77e6bd1951
Marco Solina <Marco.Solina@ixico.com>
2021-06-19 14:17:09 +0200

    refactoring

--- IxigoUi/src/main/java/com/marco/ixigo/ui/config/WebConfig.java
+++ IxigoUi/src/main/java/com/marco/ixigo/ui/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.marco.ixigo.ui.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +20,19 @@ public class WebConfig {
                 registry.addMapping("/**").allowedMethods("*");
             }
         };
+    }
+    
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }

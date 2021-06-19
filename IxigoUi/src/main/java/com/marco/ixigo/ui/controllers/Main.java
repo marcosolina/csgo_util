ac0678aefb4efd927a4bfd433c7738621cc7995f
Marco Solina <Marco.Solina@ixico.com>
2021-06-19 13:32:14 +0200

    refactoring

--- IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
+++ IxigoUi/src/main/java/com/marco/ixigo/ui/controllers/Main.java
@@ -1,10 +1,17 @@
 package com.marco.ixigo.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.marco.ixigo.ui.config.Urls;
+
 @Controller
 public class Main {
+    
+    @Autowired
+    private Urls urls;
+    
     @RequestMapping
     public String landingPage() {
         return "index";

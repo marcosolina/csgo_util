1a0d27d02bb944afe94cde08e82f912dd77c7c98
Marco Solina <Marco.Solina@ixico.com>
2021-06-24 21:37:58 +0200

    test

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -9,4 +9,9 @@ echo $IXIGO_CONFIG_SERVER_USER
 echo $IXIGO_CONFIG_SERVER_PASSW
 echo "###########################################"
 
-java -jar IxigoDemManager.jar --spring.profiles.active=docker --spring.cloud.config.uri=http://configService:8888/config --eureka.client.serviceUrl.defaultZone=http://discoveryService:8765/ixigodiscovery/eureka --spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER --spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW 
+java -jar IxigoDemManager.jar \
+--spring.profiles.active=docker \
+--spring.cloud.config.uri=http://configService:8888/config \
+--eureka.client.serviceUrl.defaultZone=http://discoveryService:8765/ixigodiscovery/eureka \
+--spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
+--spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW 
--- Scripts/Docker/setup.sh
+++ Scripts/Docker/setup.sh
@@ -14,23 +14,18 @@ sed -i -e 's/\r$//' $(find $SCRIPTS_FOLDER -type f -name "*.sh")
 echo "Postgres Configuration"
 read -p "Type the PostgreSQL user name: " POSTGRES_USER
 read -p "Type the PostgreSQL passw: " POSTGRES_PASSW
-
+echo ""
 echo "IxigoConfigServer Configuration"
 # Set the IxiGo Game server passowrd
 read -p "Type the user name for the IxigoConfigServer: " CONFIG_SERVER_USER
 read -p "Type the password for the IxigoConfigServer: " CONFIG_SERVER_PASSW
 read -p "Type the encryption key for the IxigoConfigServer: " CONFIG_ENC_KEY
 
-export IXIGO_POSTGRES_USER=$POSTGRES_USER
-export IXIGO_POSTGRES_PASSW=$POSTGRES_PASSW
-export IXIGO_CONFIG_SERVER_USER=$CONFIG_SERVER_USER
-export IXIGO_CONFIG_SERVER_PASSW=$CONFIG_SERVER_PASSW
-export IXIGO_CONFIG_ENC_KEY=$CONFIG_ENC_KEY
-
-
-#sed -i -e "s/__IXIGO_CONFIG_SERVER_USER__/$IXIGO_CONFIG_SERVER_USER/g" $YML_FILE
-#sed -i -e "s/__IXIGO_CONFIG_SERVER_PASSW__/$IXIGO_CONFIG_SERVER_PASSW/g" $YML_FILE
-#sed -i -e "s/__IXIGO_CONFIG_ENC_KEY__/$IXIGO_CONFIG_ENC_KEY/g" $YML_FILE
+sed -i -e "s/__IXIGO_POSTGRES_USER__/$POSTGRES_USER/g" $YML_FILE
+sed -i -e "s/__IXIGO_POSTGRES_PASSW__/$POSTGRES_PASSW/g" $YML_FILE
+sed -i -e "s/__IXIGO_CONFIG_SERVER_USER__/$CONFIG_SERVER_USER/g" $YML_FILE
+sed -i -e "s/__IXIGO_CONFIG_SERVER_PASSW__/$CONFIG_SERVER_PASSW/g" $YML_FILE
+sed -i -e "s/__IXIGO_CONFIG_ENC_KEY__/$CONFIG_ENC_KEY/g" $YML_FILE
 
 docker-compose -f $YML_FILE up
 echo "Run the following command: docker-compose -f $YML_FILE up"

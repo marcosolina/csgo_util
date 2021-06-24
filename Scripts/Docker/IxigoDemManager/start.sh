b6b256cc38e7403136a6997e7c9c5a0e5248701e
Marco Solina <Marco.Solina@ixico.com>
2021-06-24 21:48:59 +0200

    refactoring

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -2,9 +2,7 @@
 
 sleep 30
 
-java -jar IxigoDemManager.jar \
---spring.profiles.active=docker \
---spring.cloud.config.uri=http://configService:8888/config \
---eureka.client.serviceUrl.defaultZone=http://discoveryService:8765/ixigodiscovery/eureka \
---spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
---spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW 
+export IXIGO_EUREKA_SERVER=http://discoveryService:8765/ixigodiscovery/eureka
+export IXIGO_CONFIG_SERVER_URI=http://configService:8888/config
+
+java -jar IxigoDemManager.jar --spring.profiles.active=docker

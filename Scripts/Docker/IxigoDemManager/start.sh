249d4a5849c1b82db82a9f82f4618d038e8c2dcf
Marco Solina <Marco.Solina@ixico.com>
2021-06-24 22:09:20 +0200

    refactoring

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -2,7 +2,4 @@
 
 sleep 30
 
-export IXIGO_EUREKA_SERVER=http://discoveryService:8765/ixigodiscovery/eureka
-export IXIGO_CONFIG_SERVER_URI=http://configService:8888/config
-
-java -jar IxigoDemManager.jar --spring.profiles.active=docker
+java -jar IxigoDemManager.jar
--- Scripts/Docker/IxigoDiscovery/start.sh
+++ Scripts/Docker/IxigoDiscovery/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sleep 5
+
 java -jar IxigoDiscovery.jar

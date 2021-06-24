709def1a40bf29a9ddc86bf39e60803bcc590747
Marco Solina <Marco.Solina@ixico.com>
2021-06-24 23:29:47 +0200

    refactoring

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -2,4 +2,6 @@
 
 sleep 30
 
-java -jar IxigoDemManager.jar
+java -jar IxigoDemManager.jar \
+--spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
+--spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW 

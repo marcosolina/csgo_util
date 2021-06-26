04e548711686910afec30cbbbbe8369c8a396262
Marco Solina <Marco.Solina@ixico.com>
2021-06-26 12:36:35 +0200

    refactoring

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-sleep 30
+sleep 20
+
+echo "####################"
+echo $IXIGO_CONFIG_SERVER_USER
+echo $IXIGO_CONFIG_SERVER_PASSW
+echo $IXIGO_EUREKA_SERVER
+echo $IXIGO_POSTGRES_USER
+echo $IXIGO_POSTGRES_PASSW
+ping -c 4 configservice
+echo "####################"
 
 java -jar IxigoDemManager.jar \
+--spring.cloud.config.uri=$IXIGO_CONFIG_SERVER_URI \
 --spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
---spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW 
+--spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW \
+--spring.datasource.username=$IXIGO_POSTGRES_USER \
+--spring.datasource.username=$IXIGO_POSTGRES_PASSW 

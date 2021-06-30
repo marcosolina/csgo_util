2ee6e55ae242219bb7b89ab7baf1ee0db0169ca0
Marco Solina <Marco.Solina@ixico.com>
2021-06-30 23:21:43 +0200

    test

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -2,15 +2,6 @@
 
 sleep 20
 
-echo "####################"
-echo $IXIGO_CONFIG_SERVER_USER
-echo $IXIGO_CONFIG_SERVER_PASSW
-echo $IXIGO_EUREKA_SERVER
-echo $IXIGO_POSTGRES_USER
-echo $IXIGO_POSTGRES_PASSW
-ping -c 4 config-service
-echo "####################"
-
 java -jar IxigoDemManager.jar \
 --spring.cloud.config.uri=$IXIGO_CONFIG_SERVER_URI \
 --spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \

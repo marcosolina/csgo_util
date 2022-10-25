5c404cda1eceed15cca796c1ad4b01284ffee5b2
Marco <Marco@MARCO-PC>
2022-10-25 20:56:11 +0200

    Refactoring

--- IxigoIntegrationTests/src/test/java/com/ixigo/integrationtests/configuration/CucumberConfigurationTest.java
+++ IxigoIntegrationTests/src/test/java/com/ixigo/integrationtests/configuration/CucumberConfigurationTest.java
@@ -6,14 +6,16 @@ import org.junit.platform.suite.api.Suite;
 
 import io.cucumber.core.options.Constants;
 
-
 // https://stackoverflow.com/questions/64587739/cucumberoptions-in-cucumber-junit-platform-engine
 @Suite
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, 
+@ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,
+// @formatter:off
 value = "com.ixigo.integrationtests.configuration,"
 		+ "com.ixigo.integrationtests.steps.demmanager")
-@ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-public class CucumberConfiguration {
-
+// @formatter:on
+public class CucumberConfigurationTest {
+// Keep the suffic "Test" in the class name otherwise it will not picked up by maven test
 }

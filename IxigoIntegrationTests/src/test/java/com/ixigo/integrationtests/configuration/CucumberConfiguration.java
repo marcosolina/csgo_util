dbbd1c77646f72485f353802ed3a95252fe7a9b6
Marco <Marco@MARCO-PC>
2022-10-25 20:49:18 +0200

    Refactorng

--- IxigoIntegrationTests/src/test/java/com/ixigo/integrationtests/configuration/CucumberConfiguration.java
+++ IxigoIntegrationTests/src/test/java/com/ixigo/integrationtests/configuration/CucumberConfiguration.java
@@ -6,9 +6,13 @@ import org.junit.platform.suite.api.Suite;
 
 import io.cucumber.core.options.Constants;
 
+
+// https://stackoverflow.com/questions/64587739/cucumberoptions-in-cucumber-junit-platform-engine
 @Suite
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.ixigo.integrationtests")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, 
+value = "com.ixigo.integrationtests.configuration,"
+		+ "com.ixigo.integrationtests.steps.demmanager")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 public class CucumberConfiguration {
 

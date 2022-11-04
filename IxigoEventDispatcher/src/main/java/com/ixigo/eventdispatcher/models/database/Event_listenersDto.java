3ffbf222f000cbd72d7141bd8300179f4323d2e0
Marco <Marco@MARCO-PC>
2022-11-04 19:33:31 +0100

    Refactoring

---
+++ IxigoEventDispatcher/src/main/java/com/ixigo/eventdispatcher/models/database/Event_listenersDto.java
@@ -0,0 +1,75 @@
+package com.ixigo.eventdispatcher.models.database;
+
+import java.time.LocalDateTime;
+
+import com.ixigo.eventdispatcher.enums.EventType;
+import com.ixigo.library.dto.IxigoDto;
+
+import lombok.experimental.FieldNameConstants;
+
+@FieldNameConstants
+public class Event_listenersDto implements IxigoDto{
+
+	private static final long serialVersionUID = 1L;
+	private String url_listener = "";
+	private EventType event_type;
+	private LocalDateTime last_failure = null;
+	private Long consecutive_failure = null;
+	private String active = "";
+	private LocalDateTime last_successful = null;
+
+	public String getUrl_listener(){
+		return this.url_listener;
+	}
+
+	public void setUrl_listener(String url_listener){
+		this.url_listener = url_listener;
+	}
+
+
+	public EventType getEvent_type(){
+		return this.event_type;
+	}
+
+	public void setEvent_type(EventType event_type){
+		this.event_type = event_type;
+	}
+
+
+	public LocalDateTime getLast_failure(){
+		return this.last_failure;
+	}
+
+	public void setLast_failure(LocalDateTime last_failure){
+		this.last_failure = last_failure;
+	}
+
+
+	public Long getConsecutive_failure(){
+		return this.consecutive_failure;
+	}
+
+	public void setConsecutive_failure(Long consecutive_failure){
+		this.consecutive_failure = consecutive_failure;
+	}
+
+
+	public String getActive(){
+		return this.active;
+	}
+
+	public void setActive(String active){
+		this.active = active;
+	}
+
+
+	public LocalDateTime getLast_successful(){
+		return this.last_successful;
+	}
+
+	public void setLast_successful(LocalDateTime last_successful){
+		this.last_successful = last_successful;
+	}
+
+
+}

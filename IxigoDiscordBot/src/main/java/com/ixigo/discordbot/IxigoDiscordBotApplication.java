5f3825b7814e678f6348ab9bcfc9b555480f15ad
Marco <Marco@MARCO-PC>
2022-12-03 08:40:51 +0100

    refactoring

--- IxigoDiscordBot/src/main/java/com/ixigo/discordbot/IxigoDiscordBotApplication.java
+++ IxigoDiscordBot/src/main/java/com/ixigo/discordbot/IxigoDiscordBotApplication.java
@@ -1,13 +1,13 @@
 package com.ixigo.discordbot;
 
-/**
- * Hello world!
- *
- */
-public class IxigoDiscordBotApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class IxigoDiscordBotApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IxigoDiscordBotApplication.class, args);
+	}
 }

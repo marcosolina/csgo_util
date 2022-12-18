7778b252a88a5936bf6cda85f83f7b826ad36155
marcosolina@gmail.com <marcosolina@gmail.com>
2022-12-18 19:02:56 +0100

    refactoring

--- Scripts/Docker/IxigoDemManager/start.sh
+++ Scripts/Docker/IxigoDemManager/start.sh
@@ -3,4 +3,9 @@
 # Give some time to the Eureka container and config container to start
 sleep 20
 
-java -jar IxigoDemManager.jar
+java -jar IxigoDemManager.jar \
+--spring.cloud.config.uri=$IXIGO_CONFIG_SERVER_URI \
+--spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
+--spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW \
+--spring.datasource.username=$POSTGRES_USER \
+--spring.datasource.password=$POSTGRES_PASSWORD 

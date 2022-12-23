2e074e78ed2a10432e425b01c2c6bfcf3a22e91e
marcosolina@gmail.com <marcosolina@gmail.com>
2022-12-23 08:33:40 +0100

    refactoring

--- Scripts/Docker/IxigoDiscordBot/start.sh
+++ Scripts/Docker/IxigoDiscordBot/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sleep 35
+sleep 20
 
-java -jar IxigoDiscordBot.jar \
---spring.cloud.config.uri=$IXIGO_CONFIG_SERVER_URI \
---spring.cloud.config.username=$IXIGO_CONFIG_SERVER_USER \
---spring.cloud.config.password=$IXIGO_CONFIG_SERVER_PASSW \
---spring.datasource.username=$IXIGO_POSTGRES_USER \
---spring.datasource.password=$IXIGO_POSTGRES_PASSW 
+java -jar IxigoDiscordBot.jar

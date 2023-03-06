cd5a209002b90c53d60dd6f293d45cff2f8cc2f1
Marco Solina <marcosolina@gmail.com>
2023-03-06 07:59:44 +0100

    refactoring

--- IxigoRconApiContract/src/main/java/com/ixigo/rconapi/models/rest/RestRconResponse.java
+++ IxigoRconApiContract/src/main/java/com/ixigo/rconapi/models/rest/RestRconResponse.java
@@ -2,10 +2,13 @@ package com.ixigo.rconapi.models.rest;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.annotations.ApiModelProperty;
 
 public class RestRconResponse  implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@JsonProperty("rcon_response")
 	@ApiModelProperty(notes = "The RCON response (if any)", required = false)
     private String rconResponse;
 

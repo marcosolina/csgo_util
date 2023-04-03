7ab0ed7e429ee48f055e5bb6835f16b5369698c3
Marco Solina <marcosolina@gmail.com>
2023-04-03 18:04:49 +0200

    refactoring

--- IxigoUi/src/reactapp/src/contents/charts/ChartsContent.tsx
+++ IxigoUi/src/reactapp/src/contents/charts/ChartsContent.tsx
@@ -1,5 +1,13 @@
+import { Box } from "@mui/material";
+import { UI_CONTEXT_PATH } from "../../lib/constants";
+import wkp from "../../assets/pictures/work-in-progress.png";
+
 const ChartsContent = () => {
-  return <div>ChartsContent</div>;
+  return (
+    <Box sx={{ width: "100%" }}>
+      <img src={`${UI_CONTEXT_PATH}${wkp}`} width={"100%"} alt="" />
+    </Box>
+  );
 };
 
 export default ChartsContent;

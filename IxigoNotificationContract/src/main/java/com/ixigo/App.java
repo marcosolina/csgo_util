23fed9a915da2ac144e45e8b1bad80f817f6fb09
Marco Solina <marcosolina@gmail.com>
2023-06-20 07:09:03 +0200

    notification service

---
+++ IxigoNotificationContract/src/main/java/com/ixigo/App.java
@@ -0,0 +1,13 @@
+package com.ixigo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

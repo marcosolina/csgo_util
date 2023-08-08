8bcbaf25a11bcc8b5aebae4dd71cfb7be68c3b64
Mark Austin <mark.r.austin@gmail.com>
2023-08-08 23:20:31 +0100

    Added some indexes to the DB to improve performace slightly

--- Scripts/Docker/PostgreSql/015-initNewDemFilesDb.sql
+++ Scripts/Docker/PostgreSql/015-initNewDemFilesDb.sql
@@ -1283,3 +1283,42 @@ LEFT JOIN
     PLAYER_WEAPON_RANKING w1 ON o.steamid=w1.steamid AND w1.weapon_rank = 1
 LEFT JOIN
     PLAYER_WEAPON_RANKING w2 ON o.steamid=w2.steamid AND w2.weapon_rank = 2;
+
+
+CREATE INDEX idx_match_id_player_round_stats ON PLAYER_ROUND_STATS(match_id);
+CREATE INDEX idx_steam_id_player_round_stats ON PLAYER_ROUND_STATS(steamID);
+CREATE INDEX idx_round_player_round_stats ON PLAYER_ROUND_STATS(round);
+
+CREATE INDEX idx_match_id_round_events ON ROUND_EVENTS(match_id);
+CREATE INDEX idx_steam_id_round_events ON ROUND_EVENTS(steamID);
+CREATE INDEX idx_round_round_events ON ROUND_EVENTS(round);
+
+CREATE INDEX idx_match_id_round_kill_events ON ROUND_KILL_EVENTS(match_id);
+CREATE INDEX idx_steam_id_round_kill_events ON ROUND_KILL_EVENTS(steamID);
+CREATE INDEX idx_round_round_kill_events ON ROUND_KILL_EVENTS(round);
+
+CREATE INDEX idx_match_id_round_shot_events ON ROUND_SHOT_EVENTS(match_id);
+CREATE INDEX idx_steam_id_round_shot_events ON ROUND_SHOT_EVENTS(steamID);
+CREATE INDEX idx_round_round_shot_events ON ROUND_SHOT_EVENTS(round);
+
+CREATE INDEX idx_match_id_round_hit_events ON ROUND_HIT_EVENTS(match_id);
+CREATE INDEX idx_steam_id_round_hit_events ON ROUND_HIT_EVENTS(steamID);
+CREATE INDEX idx_round_round_hit_events ON ROUND_HIT_EVENTS(round);
+
+CREATE INDEX idx_match_id_player_stats ON PLAYER_STATS(match_id);
+CREATE INDEX idx_steam_id_player_stats ON PLAYER_STATS(steamID);
+
+CREATE INDEX idx_match_id_round_stats ON ROUND_STATS(match_id);
+
+
+CREATE INDEX idx_player_round_stats_composite_killer ON PLAYER_ROUND_STATS(steamid, match_id, round);
+CREATE INDEX idx_round_events_composite ON ROUND_EVENTS(steamID, match_id, round);
+CREATE INDEX idx_round_kill_events_composite ON ROUND_KILL_EVENTS(steamID, match_id, round);
+CREATE INDEX idx_round_shot_events_composite ON ROUND_SHOT_EVENTS(steamID, match_id, round);
+CREATE INDEX idx_round_hit_events_composite ON ROUND_HIT_EVENTS(steamID, match_id, round);
+
+CREATE INDEX idx_player_round_stats_steamid_matchid ON PLAYER_ROUND_STATS(steamID, match_id);
+CREATE INDEX idx_round_events_steamid_matchid ON ROUND_EVENTS(steamID, match_id);
+CREATE INDEX idx_round_kill_events_steamid_matchid ON ROUND_KILL_EVENTS(steamID, match_id);
+CREATE INDEX idx_round_shot_events_steamid_matchid ON ROUND_SHOT_EVENTS(steamID, match_id);
+CREATE INDEX idx_round_hit_events_steamid_matchid ON ROUND_HIT_EVENTS(steamID, match_id);

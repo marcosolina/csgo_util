3b85bfe704835997e1214e97771354d3ff83ee8d
Marco Solina <marcosolina@gmail.com>
2023-08-25 22:44:38 +0200

    updates

--- IxigoUi/src/reactapp/src/contents/stats/StatsContent.tsx
+++ IxigoUi/src/reactapp/src/contents/stats/StatsContent.tsx
@@ -23,7 +23,7 @@ const StatsContent = () => {
   const paths = useMemo(() => {
     const paths = [];
     const startSlice = UI_CONTEXT_PATH === "" ? 0 : 1;
-    for (let i = 0; i < pathnames.length; i++) {
+    for (let i = startSlice; i < pathnames.length; i++) {
       paths.push(`/${pathnames.slice(startSlice, i + 1).join("/")}`);
     }
     return paths;
